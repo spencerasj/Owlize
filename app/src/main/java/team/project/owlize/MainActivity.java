3c795085f72d88c27d981947fd6f671d857a8cc5
spencerasj <spe19021@byui.edu>
2021-03-09 13:41:33 -0700

    adding the MainActivity button and title.  Created the Dashboard activity and created an onclick to open Dashboard from the Main.

--- app/src/main/java/team/project/owlize/MainActivity.java
+++ app/src/main/java/team/project/owlize/MainActivity.java
@@ -1,15 +1,32 @@
 package team.project.owlize;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Test");
+
+        // Create button to start the app
+        Button getStartedBtn = (Button) findViewById(R.id.getStartedBtn);
+        getStartedBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDashboard();
+            }
+        });
+    }
+
+    private void openDashboard() {
+        Intent intent = new Intent(this, Dashboard.class);
+        startActivity(intent);
     }
 }
