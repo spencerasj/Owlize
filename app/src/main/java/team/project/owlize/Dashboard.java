3fad45ef6bcb3c31cba2da30259c7ecae6d5e069
spencerasj <spe19021@byui.edu>
2021-03-23 09:40:24 -0600

    added activities and tied to xml

--- app/src/main/java/team/project/owlize/Dashboard.java
+++ app/src/main/java/team/project/owlize/Dashboard.java
@@ -1,6 +1,9 @@
 package team.project.owlize;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,17 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+           // Create button to Add Content
+        Button addContentButton = (Button) findViewById(R.id.addContentBtn);
+        addContentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSemesterActivity();
+            }
+        });
     }
-}
+    private void openSemesterActivity() {
+        Intent intent = new Intent(this, SemesterActivity.class);
+        startActivity(intent);
+    }
+    }
---
+++ app/src/main/java/team/project/owlize/SemesterActivity.java
@@ -0,0 +1,14 @@
+package team.project.owlize;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SemesterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_semester);
+    }
+}
