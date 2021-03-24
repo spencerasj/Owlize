be56861a230012105196d98b92570b73c5b972e8
Jeremy Petersen <pet19037@byui.edu>
2021-03-23 18:56:16 -0700

    HTTPHelper for API

---
+++ app/src/main/java/team/project/owlize/HTTPHelper.java
@@ -0,0 +1,39 @@
+package team.project.owlize;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HTTPHelper {
+    public StringBuffer readHTTP(String url, String token) {
+        StringBuffer response = new StringBuffer();
+        try {
+            // Convert the String url to a URL object which will allow a connection to be established
+            URL urlObj = new URL(url);
+            // Open a Connection to the remote site.  This will send the HTTP Get request
+            HttpURLConnection connection = (HttpURLConnection) urlObj.openConnection();
+            connection.setRequestProperty("Authorization", "Bearer " + token);
+            connection.connect();
+            connection.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String output;
+
+
+            while ((output = in.readLine()) != null) {
+                response.append(output);
+            }
+
+            in.close();
+            // printing result from response
+            //System.out.println("Response:-" + response.toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+}
+
