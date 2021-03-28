34e7be13789d0b904e7a25c18f8510ce34101720
abrower10612 <abrower7@byui.edu>
2021-03-27 22:42:43 -0600

    adding manual dashboard for manual course and task entry

--- app/src/main/java/team/project/owlize/manual_dashboard.java
+++ app/src/main/java/team/project/owlize/manual_dashboard.java
@@ -3,12 +3,81 @@ package team.project.owlize;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.FileUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class manual_dashboard extends AppCompatActivity {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual_dashboard);
+        // ADD HERE
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        items = new ArrayList<String>();
+//        readItems();
+        itemsAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        // setup remove listener method call
+        setupListViewListener();
     }
+
+    // Attaches a long click listener to the listview
+    private void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(
+        new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapter, View item, int pos, long id) {
+                // Remove the item within array at position
+                items.remove(pos);
+                // Refresh the adapter
+                itemsAdapter.notifyDataSetChanged();
+//                writeItems();
+                // Return true consumes the long click event (marks it handled)
+                return true;
+            }
+
+        });
+    }
+
+    public void onAddItem(View v) {
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+//        writeItems();
+    }
+
+//    private void readItems() {
+//        File filesDir = getFilesDir();
+//        File todoFile = new File(filesDir, "todo.txt");
+//        try {
+//            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+//        } catch (IOException e) {
+//            items = new ArrayList<String>();
+//        }
+//    }
+//
+//    private void writeItems() {
+//        File filesDir = getFilesDir();
+//        File todoFile = new File(filesDir, "todo.txt");
+//        try {
+//            FileUtils.writeLines(todoFile, items);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
